66a65208b4293177abc4ca1f40e1fff35acf797e
pratik verma <pratikkumar99469@gmail.com>
2026-04-23 14:58:44 +0530

    Fix Vercel 404 by combining dist builds directive with rewrites

--- portfollio-main/vite.config.mjs
+++ portfollio-main/vite.config.mjs
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 this fixes the blank page on deployment
-  build: {
-    outDir: 'build', // Tell Vite to output to 'build' folder to match Vercel's expectation
-  }
 })
